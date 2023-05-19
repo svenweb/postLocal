eebf27cee372ba3b8d02734725ca1eb548a0c9fc
WalkerRout <walkerrout04@gmail.com>
2023-05-18 17:22:56 -0700

    lib/.gitignore removed, ./launch.sh reset now installs library

--- launch.sh
+++ launch.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-cd utility
-
-cargo build --release
-cp target/release/libutility.so ../lib
-mv ../lib/libutility.so ../lib/utility.so
-
-cd ..
-
 # determine whether to run the actual application or integration tests
 if [ "$1" == "reset" ]
 then
   echo "Reset -> reinstalling requirements..."
   pip3 install -r requirements.txt
+
+  echo "Reset -> reinstalling rust modules..."
+  cd utility
+
+  cargo build --release
+  cp target/release/libutility.so ../lib
+  mv ../lib/libutility.so ../lib/utility.so
+
+  cd ..
 elif [ "$1" == "run" ]
 then
   python3 src/main.py
